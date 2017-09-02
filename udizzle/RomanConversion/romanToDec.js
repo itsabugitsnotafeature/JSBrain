31d48472136667b6daf99f9fc8d455b15dfa6e87
itsabugitsnotafeature <itsabugitsnotafeature@gmail.com>
2017-09-02 13:45:50 -0700

    Implementing Decimal to Roman conversion.

--- udizzle/RomanConversion/romanToDec.js
+++ udizzle/RomanConversion/romanToDec.js
@@ -1,87 +1,102 @@
 /*
 *  Function to convert Given Roman number to 
 *  its Deciman Equavalent.
-*/ 
+*/
 
-function romanToDec(romanNum){
-  let eachRoman = romanNum.split(''),
-      result = 0;
-  const ALL_ROMANS = {
-    'i': 1,
-    'x': 10,
-    'v': 5
-  };
-  for(let i=0; i<eachRoman.length; i++ ){
-    let temp = eachRoman[i].toLowerCase();
-    if(temp in ALL_ROMANS){
-      if(temp === 'i' && i!= (eachRoman.length-1) ){
-        result -= 1;
-        continue;
-      } else {
-        result += ALL_ROMANS[temp];  
-      }
-    } else { // Invalid roman literal
-      console.log("ERROR: Invalid roman number.");
-      result = null;
+// SIMPLE ASSERT FUNCTION
+function assert(isValue, expectedValue) {
+    if (!(isValue === expectedValue)) {
+        console.log("Expected calculated value:" + isValue +
+            " to be Equal to:" + expectedValue);
+        return false;
     }
-  }
-  return result;
+    return true;
 }
 
-function assert(isValue, expectedValue){
-  if(!(isValue === expectedValue)){
-    console.log("Expected calculated value:" + isValue + 
-                " to be Equal to:" + expectedValue);
-    return false;
-  } 
-  return true;
-}
-
-function fromRoman(str) {  
-  let result = 0;
-  // the result is now a number, not a string
-  let decimal = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
-  let roman = ["M", "CM","D","CD","C", "XC", "L", "XL", "X","IX","V","IV","I"];
-
-  for (let i = 0;i<=decimal.length;i++) {
-//     debugger
-    while (str.indexOf(roman[i]) === 0){
-      
-      result += decimal[i];
-      str = str.replace(roman[i],'');
-      
+function romanToDec(str) {
+    debugger
+    if( !str || (typeof str != 'string') || (str === '')) {
+        return null;
     }
-    
-  }
-  
-  return result;
+    str = str.toUpperCase();
+
+    let result = 0;
+    let decimal = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];                   // The result is now a number, not a string
+    let roman = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];    //   THIS IS WHERE ALL THE MAGIC HAPPENS
+
+    for (let i = 0; i <= decimal.length; i++) {
+        while (str.indexOf(roman[i]) === 0) {
+            result += decimal[i];
+            str = str.replace(roman[i], '');
+        }
+    }
+    return result;
 }
-debugger
-console.log( assert(fromRoman("I"), 1) );
-console.log( assert(fromRoman("V"), 5) );
-console.log( assert(fromRoman("X"), 10) );
-console.log( assert(fromRoman("IV"), 4) );
-console.log( assert(fromRoman("IX"), 9) );
 
-console.log( assert(fromRoman("XXX"), 30) );
-console.log( assert(fromRoman("XXIII"), 23) );
-console.log( assert(fromRoman("XIV"), 14) );
-console.log( assert(fromRoman("XXXVIII"), 38) );
-console.log( assert(fromRoman("XXIX"), 29) );
+let decToRoman = (num) => {
+    if( !num || isNaN(num) || (num === 0)  || (num < 0))  {
+        return null;
+    }
+
+    let result = '';
+    let decimal = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+    let roman = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
+    debugger
+    while ( num > 0 ) {
+        for(let i=0; i<decimal.length; i++){
+            if( num === decimal[i]){
+                result += roman[i];
+                num = 0;
+            } else if (num > decimal[i]){
+                result += roman[i];
+                num -= decimal[i];
+                i=0;
+            } else {
+                continue;
+            }
+        }
+    }
+    return result;
+}
 
 
 
-// console.log( assert(romanToDec("i"), 1) );
-// console.log( assert(romanToDec("V"), 5) );
-// console.log( assert(romanToDec("x"), 10) );
-// console.log( assert(romanToDec("iv"), 4) );
-// console.log( assert(romanToDec("ix"), 9) );
+/* 
+ *    TC for converting 
+ *      Decimal Number  ->>  Roman Numeral
+ */ 
 
-// console.log( assert(romanToDec("xxx"), 30) );
-// console.log( assert(romanToDec("xxIII"), 23) );
-// console.log( assert(romanToDec("XIV"), 14) );
-// console.log( assert(romanToDec("XXXVIII"), 38) );
-// console.log( assert(romanToDec("xxix"), 29) );
+console.log( assert(decToRoman(117), "CXVII"));
+console.log( assert(decToRoman(30), "XXX"));
+console.log( assert(decToRoman(23), "XXIII"));
+console.log( assert(decToRoman(14), "XIV"));
+console.log( assert(decToRoman(38), "XXXVIII"));
+console.log( assert(decToRoman(29), "XXIX"));
+console.log( assert(decToRoman(0), null));
+console.log( assert(decToRoman(23),'XXIII'));
+console.log( assert(decToRoman(4),'IV'));
+console.log( assert(decToRoman(1),'I'));
+console.log( assert(decToRoman(5),'V'));
 
+ 
 
+/* 
+ *    TC for converting 
+ *      Roman Numeral  ->> Decimal Number 
+ */
 
+/* console.log( assert(romanToDec("I"), 1));
+console.log( assert(romanToDec("V"), 5));
+console.log( assert(romanToDec("X"), 10));
+console.log( assert(romanToDec("IV"), 4));
+console.log( assert(romanToDec("IX"), 9));
+console.log( assert(romanToDec("XXX"), 30));
+console.log( assert(romanToDec("XXIII"), 23));
+console.log( assert(romanToDec("XIV"), 14));
+console.log( assert(romanToDec("XXXVIII"), 38));
+console.log( assert(romanToDec("XXIX"), 29));
+console.log( assert(romanToDec("i"), 1) );
+console.log( assert(romanToDec("V"), 5) );
+console.log( assert(romanToDec("x"), 10) );
+console.log( assert(romanToDec("iv"), 4) );
+console.log( assert(romanToDec("ix"), 9) ); */
