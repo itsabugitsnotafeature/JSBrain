92f9e3930b8eacdcba1330b2f2d24321e57e4f80
itsabugitsnotafeature <itsabugitsnotafeature@gmail.com>
2017-09-15 20:52:54 -0700

    My solution !!

--- udizzle/Numbers/AtoI.js
+++ udizzle/Numbers/AtoI.js
@@ -6,7 +6,6 @@
 // aToI("-984") => -984
 // aToI("-3.14") => -3.14
 
-
 function aToI(str) {
     if( typeof str !== 'string' ){
         return null;
@@ -22,3 +21,34 @@ console.log(aToI("1"));
 console.log(aToI("43"));
 console.log(aToI("541"));
 console.log(aToI("-43"));
+
+
+
+// ITERATIVE SOLUTION 
+
+function aToI(str){
+    let result = 0;
+    let num;
+    let isNegative = false;
+    let i = 0;
+    
+    if(str[0] === '-'){
+      i = 1;
+      isNegative = true;
+    }
+    
+    for(; i<=(str.length -1) ; i++ ){
+      num = (str[i].charCodeAt(0) - 48);
+      result += (num * (Math.pow(10,(str.length - i - 1))))
+    }
+    if(isNegative) {
+      result *= (-1);
+    }
+    return result;
+  }
+  
+  console.log(aToI("-78349"));
+  console.log(aToI("-111"));
+  console.log(aToI("111"));
+  console.log(aToI("9876543210"));
+  
