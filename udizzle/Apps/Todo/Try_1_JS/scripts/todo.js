ca3e50537ec3f025decfd2c41a4e094a9498bc66
itsabugitsnotafeature <itsabugitsnotafeature@gmail.com>
2017-09-20 23:06:10 -0700

    Revamped design. Ability to Add and Remove ToDo.

--- udizzle/Apps/Todo/Try_1_JS/scripts/todo.js
+++ udizzle/Apps/Todo/Try_1_JS/scripts/todo.js
@@ -1,11 +1,73 @@
 'use strict'
 
+function createCloseButton() {
+    // Create a "close" button and append it to each list item
+    let myNodelist = document.getElementsByTagName("li");
+    
+    for (let i = 0; i < myNodelist.length; i++) {
+        let span = document.createElement("span");
+        let txt = document.createTextNode("\u00D7");
+        span.className = "close";
+        span.appendChild(txt);
+        myNodelist[i].appendChild(span);
+    }
+}
 
-function addTodo() {
-    let todoList = document.getElementById('todo-list');
-    let todoItem = document.getElementById('todo-item').value;
-    let entry = document.createElement('li');
-    entry.appendChild(document.createTextNode(todoItem));
-    todoList.appendChild(entry);
-};
 
+function deleteTodo() {
+    // Click on a close button to hide the current list item
+    let close = document.getElementsByClassName("close");
+    
+    for (let i = 0; i < close.length; i++) {
+        close[i].onclick = function () {
+            let div = this.parentElement;
+            div.style.display = "none";
+        }
+    }
+}
+
+function markComplete() {
+    // Add a "checked" symbol when clicking on a list item
+    let list = document.querySelector('ul');
+    list.addEventListener('click', function (ev) {
+        if (ev.target.tagName === 'li') {
+            ev.target.classList.toggle('checked');
+        }
+    }, false);
+}
+
+
+// Create a new list item when clicking on the "Add" button
+function newElement() {
+    let li = document.createElement("li");
+    let inputValue = document.getElementById("todoItem").value;
+    let t = document.createTextNode(inputValue);
+
+    li.appendChild(t);
+    if (inputValue === '') {
+        alert("You must write something!");
+        console.error("You must write something!");
+    } else {
+        document.getElementById("myTodoList").appendChild(li);
+    }
+    
+    document.getElementById("todoItem").value = "";
+
+    let span = document.createElement("span");
+    let txt = document.createTextNode("\u00A0\u00A0 \u00D7 \u00A0\u00A0");
+    
+    span.className = "close";
+    span.appendChild(txt);
+    span.onclick = function() {
+        deleteTodo();
+      }
+
+    li.appendChild(span);
+
+    for (let i = 0; i < close.length; i++) {
+        close[i].onclick = function () {
+            let div = this.parentElement;
+            div.style.display = "none";
+        }
+    }
+}
