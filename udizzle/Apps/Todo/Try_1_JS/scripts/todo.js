d9ca2bde88ab73905e030d44c7acd39fa5a9383e
itsabugitsnotafeature <itsabugitsnotafeature@gmail.com>
2017-09-22 19:25:22 -0700

    Code cleanup Added CSS decoration Markup Cleanup

--- udizzle/Apps/Todo/Try_1_JS/scripts/todo.js
+++ udizzle/Apps/Todo/Try_1_JS/scripts/todo.js
@@ -3,7 +3,7 @@
 function createCloseButton() {
     // Create a "close" button and append it to each list item
     let myNodelist = document.getElementsByTagName("li");
-    
+
     for (let i = 0; i < myNodelist.length; i++) {
         let span = document.createElement("span");
         let txt = document.createTextNode("\u00D7");
@@ -13,35 +13,25 @@ function createCloseButton() {
     }
 }
 
-
-function deleteTodo() {
-    // Click on a close button to hide the current list item
-    let close = document.getElementsByClassName("close");
-    
-    for (let i = 0; i < close.length; i++) {
-        close[i].onclick = function () {
-            let div = this.parentElement;
-            div.style.display = "none";
-        }
-    }
+function deleteTodo(elem) {
+    elem.style.display = "none";
 }
-
-function markComplete() {
-    // Add a "checked" symbol when clicking on a list item
-    let list = document.querySelector('ul');
-    list.addEventListener('click', function (ev) {
-        if (ev.target.tagName === 'li') {
-            ev.target.classList.toggle('checked');
-        }
-    }, false);
-}
-
 
 // Create a new list item when clicking on the "Add" button
 function newElement() {
+
+    // Add a "DELETE" symbol on the RHS
     let li = document.createElement("li");
     let inputValue = document.getElementById("todoItem").value;
     let t = document.createTextNode(inputValue);
+
+    // Add a "checked" symbol when clicking on a list item
+    let list = document.querySelector('ul');
+    list.addEventListener('click', function(ev) {
+        if (ev.target.tagName.toLowerCase() === 'li') {
+            ev.target.classList.toggle('checked');
+        }
+    }, false);
 
     li.appendChild(t);
     if (inputValue === '') {
@@ -50,24 +40,17 @@ function newElement() {
     } else {
         document.getElementById("myTodoList").appendChild(li);
     }
-    
+
     document.getElementById("todoItem").value = "";
 
     let span = document.createElement("span");
     let txt = document.createTextNode("\u00A0\u00A0 \u00D7 \u00A0\u00A0");
-    
+
     span.className = "close";
     span.appendChild(txt);
     span.onclick = function() {
-        deleteTodo();
-      }
+        deleteTodo(this.parentElement);
+    }
 
     li.appendChild(span);
-
-    for (let i = 0; i < close.length; i++) {
-        close[i].onclick = function () {
-            let div = this.parentElement;
-            div.style.display = "none";
-        }
-    }
 }
