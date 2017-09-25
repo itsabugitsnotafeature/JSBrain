196bd025acdb3c9bccdcf117b5e25231e2d76d01
itsabugitsnotafeature <itsabugitsnotafeature@gmail.com>
2017-09-24 22:28:06 -0700

    Implemented Edit Todo Feature with Auto-SAVE

--- udizzle/Apps/Todo/Try_3_JS/myTodo.js
+++ udizzle/Apps/Todo/Try_3_JS/myTodo.js
@@ -1,6 +1,8 @@
 'use strict'
 
 let LOCAL_STORAGE_TODO_KEY = 'MY_TODOS';
+let AUTO_SAVE_DELAY = 2000;
+
 
 let addTodo = () => {
     let itemToAdd = document.getElementById('todo-item').value;
@@ -16,12 +18,28 @@ let addTodo = () => {
     document.getElementById('todo-item').value = '';
 }
 
+let debounce = (func, wait, immediate) => {
+    let timeout;
+    return function() {
+        let context = this,
+            args = arguments;
+        let later = function() {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        let callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
+}
+
 let _updateView = () => {
     let allCurrentTodos = _getAllCurrentTodos();
 
     let todosMarkup = '<ul>'
     for (let i = 0; i < allCurrentTodos.length; i++) {
-        let listElement = "<li> " + allCurrentTodos[i] + " <button id=" + i + " name='remove-item'> [ X ] </button></li>"
+        let listElement = "<li name='editable' contenteditable='true' > " + allCurrentTodos[i] + " <button id=" + i + " name='remove-item'> [ X ] </button></li>"
         todosMarkup += listElement;
     }
     todosMarkup += '</ul>'
@@ -30,7 +48,30 @@ let _updateView = () => {
     let allRemoveButtons = document.getElementsByName('remove-item');
     allRemoveButtons.forEach((removeButton) => {
         removeButton.addEventListener('click', removeTodoItem);
-    })
+    });
+
+    let allListItems = document.getElementsByName("editable");
+    allListItems.forEach((todoItem) => {
+        todoItem.addEventListener("input", _saveUpdatedTodo, false);
+    });
+}
+
+let _saveUpdatedTodo = debounce((event) => {
+    console.log("called _saveUpdatedTodo ");
+    _updateList(event.target.children[0].id, event.target.innerText);
+}, AUTO_SAVE_DELAY);
+
+let _updateList = (id, todoValue) => {
+    console.log("called _saveUpdatedTodo ");
+    let closeButtonIndex = todoValue.indexOf(' [ X ]');
+    todoValue = todoValue.substring(0, closeButtonIndex);
+
+    console.log("Updateing \nitem number: " + id + "\nwith value " + todoValue + "\n");
+
+    let allCurrentTodos = _getAllCurrentTodos();
+    allCurrentTodos[id] = todoValue;
+    _setAllCurrentTodos(allCurrentTodos);
+    console.log("SAVE COMPLETE !");
 }
 
 let removeTodoItem = () => {
