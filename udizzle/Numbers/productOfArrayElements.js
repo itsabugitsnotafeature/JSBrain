6f2c59e37331ac9add3f588dc55bce037f3b0e06
itsabugitsnotafeature <itsabugitsnotafeature@gmail.com>
2017-10-02 19:45:25 -0700

    Finding product of each element in array except for self.

---
+++ udizzle/Numbers/productOfArrayElements.js
@@ -0,0 +1,54 @@
+/* 
+    Source: 
+        https://github.com/kennymkchan/interview-questions-in-javascript
+    
+    Given an array of integers, 
+    return an output array such that output[i] is 
+    equal to the product of all the elements in the 
+    array other than itself. 
+    
+    (Solve this in O(n) without division)
+
+    Example: 
+        var firstArray = [2, 2, 4, 1];
+        var secondArray = [0, 0, 0, 2];
+        var thirdArray = [-2, -2, -3, 2];
+
+        productExceptSelf(firstArray); // [8, 8, 4, 16]
+        productExceptSelf(secondArray); // [0, 0, 0, 0]
+        productExceptSelf(thirdArray); // [12, 12, 8, -12]
+*/
+
+let prodArrayWithDivision = (arr) => {
+    let allProd = arr.reduce((prev,curr) => {
+        return (prev * curr);
+    }, 1)
+    let prodArr = new Array(arr.length);
+    arr.forEach(function(element, index) {
+        prodArr[index] = allProd / element;
+    }, this);
+    return prodArr;
+}
+
+let prodArrayWoDivision = (arr) => {
+    let prodArr = new Array();
+    
+    let mul = 1;
+    for(let i=0 ; i< arr.length ; i++){
+        prodArr.push(mul);
+        mul *= arr[i];
+    }
+    return prodArr;
+
+    mul = 1;
+    for(let i=arr.length-1 ; i >= 0 ; i--){
+        prodArr[i] *= mul;
+        mul *= arr[i];
+    }
+
+    return prodArr;
+}
+
+let arr = [2, 2, 4, 1];
+console.log( JSON.stringify(prodArrayWoDivision(arr)));
+// console.log( JSON.stringify(prodArrayWithDivision(arr)));
