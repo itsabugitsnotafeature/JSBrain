505d4d94bfbd2286481bc50efd2c4ca183562c0e
itsabugitsnotafeature <itsabugitsnotafeature@gmail.com>
2017-10-10 07:27:21 -0700

    Added Mixins and Closures

--- udizzle/portotypeConcepts.js
+++ udizzle/portotypeConcepts.js
@@ -1,14 +1,98 @@
 'use strict'
 
+/* 
+    MIXINS 
+    Concepts : 
+    Mixin – is a generic object-oriented programming term: 
+        a class that contains methods for other classes.
+    JavaScript does not support multiple inheritance, 
+        but mixins can be implemented by copying them into the prototype.
+    We can use mixins as a way to augment a class by multiple behaviors, 
+        like event-handling as we have seen above.
+    Mixins may become a point of conflict 
+        if they occasionally overwrite native class methods. 
+    So generally one should think well about the 
+        naming for a mixin, to minimize such possibility.
+*/
+
+console.log("\n EX-3");
+
+let sayMixin = {
+    say(phrase){
+        console.log(phrase);
+    }
+}
+
+let sayHiMixin = {
+    __proto__: sayMixin,
+    sayHi(name){
+        super.say("Hello " + this.name);
+    },
+    sayBye(name){
+        super.say("Bye " + this.name);
+    }
+}
+
+class User_mixin_1{
+    constructor(name){
+        this.name = name;
+    }
+}
+
+Object.assign(User_mixin_1.prototype, sayHiMixin);
+let u1 = new User_mixin_1("YouDizzle");
+u1.sayHi();
+u1.sayBye();
+
+new User_mixin_1("Dizz-le").sayHi();
+
+
 
 
 /* 
     CLOSURES EXAMPLES 
+    Concepts : 
+        ** L-E-R: 
+            Lexical Environment 
+            Environment Record
+
+        ** Function can Access: => Closure => Lexical Scoping
+            Inner Lexical Environment
+            Outer Lexical Enviroment 
+        
+         CLOSURE : 
+            is a function that remembers its outer variables 
+                and can access them. 
+
+            “what’s a closure?”, 
+                a valid answer would be a definition of the closure 
+                and 
+                an explanation that all functions in JavaScript are closures, 
+                and maybe few more words about technical details: 
+                the [[Environment]] property and how Lexical Environments work.
+
+
+
 */
 
+console.log("\n EX-2");
+function makeWorker(){ 
+    let name1 = "Pete";
+    return function(){ 
+        console.log(name1);
+    }
+};
+let name1 = "John";// create a function
+let work = makeWorker();// call it
+work();
+
+
+
+console.log("\n EX-1");
 let name = "John";
+console.log(sayHi());
 function sayHi(){ 
-    console.log("Hi, " + name); 
+    return("Hi, " + name); 
 } 
 name = "Pete";
 console.log(sayHi());
@@ -16,10 +100,18 @@ console.log(sayHi());
 
 
 
-console.log(" PROTOTYPES EXAMPLES ");
+
+
+
+
+
+
+
+
 /* 
-    PROTOTYPES EXAMPLES 
+PROTOTYPES EXAMPLES 
 */
+console.log("\n\nPROTOTYPES EXAMPLES ");
 
 let User = {
     firstName: 'John',
