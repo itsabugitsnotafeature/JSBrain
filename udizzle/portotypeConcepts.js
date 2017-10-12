94e58f36da410a466bc2b5f71859b5507374b1a1
itsabugitsnotafeature <itsabugitsnotafeature@gmail.com>
2017-10-11 21:32:37 -0700

    Additional implementation to further explain prototype concept.

--- udizzle/portotypeConcepts.js
+++ udizzle/portotypeConcepts.js
@@ -97,8 +97,31 @@ function sayHi(){
 name = "Pete";
 console.log(sayHi());
 
+// EXAMPLE 2 
 
-
+(function outerFunc(outerArg) {
+    var outerVar = 3;
+  
+    (function middleFunc(middleArg) {
+      var middleVar = 4;
+  
+      (function innerFunc(innerArg) {
+        var innerVar = 5;
+        // EXAMPLE OF SCOPE IN CLOSURE:
+        // Variables from innerFunc, middleFunc, and outerFunc,
+        // as well as the global namespace, are ALL in scope here.
+        console.log("outerArg="+outerArg+
+                    " middleArg="+middleArg+
+                    " innerArg="+innerArg+"\n"+
+                    " outerVar="+outerVar+
+                    " middleVar="+middleVar+
+                    " innerVar="+innerVar);
+        // --------------- THIS WILL LOG: ---------------
+        //    outerArg=123 middleArg=456 innerArg=789
+        //    outerVar=3 middleVar=4 innerVar=5
+      })(789);
+    })(456);
+  })(123);
 
 
 
