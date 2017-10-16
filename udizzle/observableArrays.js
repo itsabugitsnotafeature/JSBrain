b14122577002c6cbdcb3e4ba89f95f05b1edb9dd
itsabugitsnotafeature <itsabugitsnotafeature@gmail.com>
2017-10-15 18:42:26 -0700

    Implemented observable arrays.

---
+++ udizzle/observableArrays.js
@@ -0,0 +1,20 @@
+'use strict'
+
+var MyObservableArray = function(callback) {
+    var arr = [];
+    arr.push = function() {
+        callback(arguments);
+        return Array.prototype.push.apply(this, arguments);
+    }
+    return arr;
+};
+
+let callbackFunc = (event) => {
+  console.log('called callback');
+  console.log("event", event);
+    console.log('DONE with callback!\n');
+}
+
+var arr = new MyObservableArray(callbackFunc);
+arr.push(12, 3, 45);
+console.log(arr, arr[2]);
