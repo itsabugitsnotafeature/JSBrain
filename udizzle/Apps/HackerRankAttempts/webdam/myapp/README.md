60477fad69a1f425f35fb800b3e80dea929b36e1
itsabugitsnotafeature <itsabugitsnotafeature@gmail.com>
2017-10-22 15:53:57 -0700

    cleanup and added button

--- udizzle/Apps/HackerRankAttempts/webdam/myapp/README.md
+++ udizzle/Apps/HackerRankAttempts/webdam/myapp/README.md
@@ -1,5 +1,11 @@
 # myapp
-My cool app
+My cool WebDam app
+
+## DEV Notes: 
+1. Incomplete "AZ" font icon should toggle behavior implementation. 
+2. Actual JSON AJAX request made to JSON hostin g service myjson.
+URL: https://api.myjson.com/bins/1773un
+
 
 ## Install It
 ```
@@ -38,6 +44,7 @@ cf push myapp
 * Invoke the example REST endpoint `curl http://localhost:3000/api/v1/examples`
    
 
+
 ### Source for generator app
-Special Thanks :)
+Special Thanks to :)
 https://github.com/cdimascio/generator-express-no-stress
--- udizzle/Apps/HackerRankAttempts/webdam/myapp/public/js/ui-excercise.js
+++ udizzle/Apps/HackerRankAttempts/webdam/myapp/public/js/ui-excercise.js
@@ -17,34 +17,41 @@ function renderPage() {
 }
 
 function setupPage() {
-	console.log("Received JSON DATA..");	
-	let cardData = JSON.parse(this.responseText);
+	console.log("Received JSON DATA..");
+	var cardData = JSON.parse(this.responseText);
+	window.jsonData = cardData;
 
-	cardData.forEach(function(cardInfo, index) {
-		
-		let templeteNode = document.querySelector('#card-template')
-		let clone = templeteNode.content.firstElementChild;
-		
-		let tDiv = clone.cloneNode(true);
-		
+	cardData.forEach(function (cardInfo, index) {
+
+		var templeteNode = document.querySelector('#card-template')
+		var clone = templeteNode.content.firstElementChild;
+
+		var tDiv = clone.cloneNode(true);
+
 		tDiv.firstElementChild.setAttribute('src', ('./images/' + cardInfo.image_name));
 		tDiv.firstElementChild.setAttribute('alt', toCamelCase(cardInfo.title));
 		tDiv.querySelector('b').innerHTML = toCamelCase(cardInfo.title);
 		tDiv.querySelector('p').innerHTML = cardInfo.description;
 
-		let cardGrid = document.querySelector("#card-content-items");
+		var cardGrid = document.querySelector("#card-content-items");
 		cardGrid.appendChild(tDiv);
 	}, this);
 
 }
 
-function toCamelCase(str)
-{	str = str.split(' ');
-	let normalizedTitle = [];
-	str.forEach((word)=>{
+function toCamelCase(str) {
+	str = str.split(' ');
+	var normalizedTitle = [];
+	str.forEach((word) => {
 		normalizedTitle.push(word.charAt(0).toUpperCase() + word.substr(1).toLowerCase());
 	})
 	return normalizedTitle.join(' ');
+}
+
+function arrangeByTitle() {
+	var chartData = window.jsonData;
+	console.log('Rearranging by name !')
+	return true;
 }
 
 
