048a84743bb16a59634acc190c0f7108ffdf8a07
itsabugitsnotafeature <itsabugitsnotafeature@gmail.com>
2017-10-27 06:03:13 -0700

    Practice DLL

---
+++ udizzle/DataStructuresInJS/practice/try1/DoublyLinkedList/dllTest.js
@@ -0,0 +1,13 @@
+'use strict'
+
+import DoublyLinkedList from './DoublyLinkedList';
+
+console.log("Wow !")
+
+let testList = new DoublyLinkedList(55);
+testList.addNodeToLast(66);
+testList.addNodeBeginning(44);
+testList.addNodeBeginning(33);
+testList.printAll();
+
+console.log("Wow Wow !")
