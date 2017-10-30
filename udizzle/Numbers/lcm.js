3906903164d30c9a26b3444f3fc9196e8c7767ba
itsabugitsnotafeature <itsabugitsnotafeature@gmail.com>
2017-10-29 22:39:18 -0700

    Calculating LCM of two nums and range of nums

---
+++ udizzle/Numbers/lcm.js
@@ -0,0 +1,50 @@
+'use strict'
+
+/* 
+    Calcuate Lowest Common Multiple of 2 Mumbers 
+    Example : 
+        3,4 => 12
+        12, 80 => 240
+*/
+
+let calculateLowestCommonMultipleOfTwoNumbers = (a, b) => {
+    let greatestDivisor = getGreatestCommonDivisor(a, b);
+    let product = (a * b);
+    let LCM = product / greatestDivisor;
+    return LCM;
+    // return (a * b) / getGreatestCommonDivisor(a, b);
+};
+
+
+let getGreatestCommonDivisor = (a, b) => {
+    // return !b ? a : getGreatestCommonDivisor(b, a % b);
+    if(!b){
+        return a;
+    } else {
+        let thisDiv = getGreatestCommonDivisor( b, (a % b) );
+        return thisDiv;
+    }
+}
+
+console.log( "3, 4 \t => \t" + calculateLowestCommonMultipleOfTwoNumbers(3, 4) );
+console.log( "12, 80 \t => \t" + calculateLowestCommonMultipleOfTwoNumbers(12, 80) );
+
+
+let calculateLowestCommonMultipleOfRangeOfNumbers = (a, b) => {
+    let nums = getRange(a, b);
+    let num = nums[0];
+    nums.forEach((item)=>{
+        num = calculateLowestCommonMultipleOfTwoNumbers(num, item);
+    });
+    return num;
+};
+
+let getRange = (a , b) => {
+    let ranges = new Array;
+    for(let i=a ; i<=b ; i++){
+        ranges.push(i);
+    }
+    return ranges;
+}
+
+console.log( "1, 13 \t => \t" + calculateLowestCommonMultipleOfRangeOfNumbers(1, 13) );
