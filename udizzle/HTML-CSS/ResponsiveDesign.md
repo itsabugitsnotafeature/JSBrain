5e87276984fc4a306d70cefce753fdc7b6ece060
itsabugitsnotafeature <itsabugitsnotafeature@gmail.com>
2017-11-13 20:52:14 -0800

    updates and practice

---
+++ udizzle/HTML-CSS/ResponsiveDesign.md
@@ -0,0 +1,19 @@
+
+Elements of responsive Web Desing: 
+
+
+Links: 
+1. https://developers.google.com/web/fundamentals/design-and-ux/responsive/
+
+
+- Media queries 
+    - Minimize for re-directs
+    - GZIP and file size
+    - Take acoount of screen size from viewport 
+    - Dont set width in pixesl, use viewport queries
+    
+- Layout
+    - Use responsive grid layout 
+    - 
+
+
--- udizzle/Udemy/11-essential-cofing-exercises/mine_sweeper.js
+++ udizzle/Udemy/11-essential-cofing-exercises/mine_sweeper.js
@@ -21,42 +21,43 @@
 */
 
 let isMineAt = (arr, minesLoc) => {
-    if(searchForArray(minesLoc, arr)){
+    if (searchForArray(minesLoc, arr)) {
         return 1;
     } else {
         return 0;
     }
-    
+
 }
 
-function searchForArray(haystack, needle){
+function searchForArray(haystack, needle) {
     var i, j, current;
-    for(i = 0; i < haystack.length; ++i){
-      if(needle.length === haystack[i].length){
-        current = haystack[i];
-        for(j = 0; j < needle.length && needle[j] === current[j]; ++j);
-        if(j === needle.length)
-          return true;
-      }
+    for (i = 0; i < haystack.length; ++i) {
+        if (needle.length === haystack[i].length) {
+            current = haystack[i];
+            for (j = 0; j < needle.length && needle[j] === current[j]; ++j);
+            if (j === needle.length)
+                return true;
+        }
     }
     return false;
-  }
+}
+
 
 
 let mine_sweeper = (bombs, num_rows, num_cols) => {
     let field = [];
     debugger
     // ADD MINES
-    for(let i=0 ; i<num_rows ; i++){
+    for (let i = 0; i < num_rows; i++) {
         let thisCol = [];
-        for(let j=0 ; j<num_cols ; j++){
+        for (let j = 0; j < num_cols; j++) {
             let isMine = false;
             bombs.forEach(element => {
-                if(element[0] === i && element[1] === j){
+                if (element[0] === i && element[1] === j) {
                     isMine = true;
                 }
             });
-            if(isMine){
+            if (isMine) {
                 thisCol.push(-1);
             } else {
                 thisCol.push(0);
@@ -66,21 +67,21 @@ let mine_sweeper = (bombs, num_rows, num_cols) => {
     }
 
     // ADD MINE COUNTS
-    for(let i=0 ; i<num_rows ; i++){
-        for(let j=0 ; j<num_cols ; j++){
-            if(field[i][j] === -1){
+    for (let i = 0; i < num_rows; i++) {
+        for (let j = 0; j < num_cols; j++) {
+            if (field[i][j] === -1) {
                 console.log("Mine Found at I => " + i + " / J => " + j);
                 continue;
             } else {
                 // field[i][j] = isMineAtNorth(i,j) + isMineAtSouth(i,j) + isMineAtEast(i,j) + isMineAtWest(i,j);
-                field[i][j] = isMineAt([i, j+1], bombs) + 
-                    isMineAt([i, j-1], bombs) + 
-                    isMineAt([i+1, j], bombs) + 
-                    isMineAt([i-1, j], bombs) + 
-                    isMineAt([i+1, j+1], bombs) + 
-                    isMineAt([i+1, j-1], bombs) + 
-                    isMineAt([i-1, j+1], bombs) + 
-                    isMineAt([i-1, j-1], bombs);
+                field[i][j] = isMineAt([i, j + 1], bombs) +
+                    isMineAt([i, j - 1], bombs) +
+                    isMineAt([i + 1, j], bombs) +
+                    isMineAt([i - 1, j], bombs) +
+                    isMineAt([i + 1, j + 1], bombs) +
+                    isMineAt([i + 1, j - 1], bombs) +
+                    isMineAt([i - 1, j + 1], bombs) +
+                    isMineAt([i - 1, j - 1], bombs);
             }
         }
     }
@@ -89,11 +90,14 @@ let mine_sweeper = (bombs, num_rows, num_cols) => {
 
 let config1 = {
     bombs: [
-        [0,0],
-        [0,1]
+        [0, 0],
+        [0, 1]
     ],
     num_rows: 3,
     num_cols: 4
 }
 
-console.log( JSON.stringify( mine_sweeper(config1.bombs, config1.num_rows, config1.num_cols), null, 2 ) );
+console.log(JSON.stringify(mine_sweeper(config1.bombs, config1.num_rows, config1.num_cols), null, 2));
+
+
+// TODO : Implement CLICK and expose the connected cells
