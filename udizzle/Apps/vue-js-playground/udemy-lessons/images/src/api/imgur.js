08af08c7646269c29a03285c4d2ac310aa45c161
itsabugitsnotafeature <itsabugitsnotafeature@gmail.com>
2019-10-01 23:09:01 -0700

    - Hooked up a new store for images using vuex - Added logic to fetch images on load of imagelist

--- udizzle/Apps/vue-js-playground/udemy-lessons/images/src/api/imgur.js
+++ udizzle/Apps/vue-js-playground/udemy-lessons/images/src/api/imgur.js
@@ -1,5 +1,6 @@
 'use strict';
 import qs from 'qs';
+import axios from 'axios';
 
 const CLIENT_ID = '142de37380a46ae';
 const ROOT_URL = 'https://api.imgur.com';
@@ -13,5 +14,13 @@ export default {
             response_type: 'token',
         };
         window.location = `${ROOT_URL}/oauth2/authorize?${qs.stringify(queryString)}`;
+    },
+    fetchImages(access_tokae) {
+        debugger
+        return axios.get(`${ROOT_URL}/3/account/me/images`, {
+            headers: {
+                Authorization: `Bearer ${access_tokae}`,
+            }
+        });
     },
 }
