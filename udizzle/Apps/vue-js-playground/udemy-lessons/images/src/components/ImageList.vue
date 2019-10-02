d0546c80ac5d4aac92b09959b7d97db2ea75a8c5
itsabugitsnotafeature <itsabugitsnotafeature@gmail.com>
2019-10-02 00:11:04 -0700

    Added CSS Grids to make image layout little pretty

--- udizzle/Apps/vue-js-playground/udemy-lessons/images/src/components/ImageList.vue
+++ udizzle/Apps/vue-js-playground/udemy-lessons/images/src/components/ImageList.vue
@@ -1,5 +1,5 @@
 <template>
-    <div id="image-list">
+    <div class="image-container">
         <img v-for="image in allImages" :src="image.link" :key="image.id"/>
     </div>
 </template>
@@ -18,5 +18,14 @@ export default {
 </script>
 
 <style scoped>
+.image-container {
+    column-count: 3;
+    column-gap: 0;
+}
+
+img {
+    max-width: 100%;
+    padding: 5px;
+}
 
 </style>
