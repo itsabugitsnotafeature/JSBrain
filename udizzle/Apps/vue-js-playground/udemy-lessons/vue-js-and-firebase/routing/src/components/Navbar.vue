62eb2128894e45de4698a78fba7bdf507dab452b
itsabugitsnotafeature <itsabugitsnotafeature@gmail.com>
2019-10-03 21:57:46 -0700

    Introduction to router and router links Initial commit

---
+++ udizzle/Apps/vue-js-playground/udemy-lessons/vue-js-and-firebase/routing/src/components/Navbar.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="hello">
+    <ul>
+      <li> <router-link :to="{name: 'Home'}"> Home </router-link> </li>
+      <li> <router-link :to="{name: 'About'}"> About </router-link> </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+ul {
+  list-style: none;
+  padding: 0px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
