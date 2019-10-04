d5c217eba34522ca6a4ceaf54c89f603ab8fc2f8
itsabugitsnotafeature <itsabugitsnotafeature@gmail.com>
2019-10-03 22:22:51 -0700

    Added profile route with dynamic ID that is being read. Also added watcher to check if route is being updated and accordingly update the dom message

---
+++ udizzle/Apps/vue-js-playground/udemy-lessons/vue-js-and-firebase/routing/src/components/ViewProfile.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="view-profile">
+    <h2>ViewProfile</h2>
+    <h3> User ID => {{ userId }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ViewProfile',
+  data () {
+    return {
+      userId: this.$route.params.user_id
+    }
+  },
+  watch: {
+    $route: 'updateId'
+  },
+  methods: {
+    updateId () {
+      this.userId = this.$route.params.user_id
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
