f60a10e6176f473d142e3471ca1d154324a9eb94
itsabugitsnotafeature <itsabugitsnotafeature@gmail.com>
2019-10-03 22:31:44 -0700

    Dynamic link creation paired with routing

--- udizzle/Apps/vue-js-playground/udemy-lessons/vue-js-and-firebase/routing/src/components/Navbar.vue
+++ udizzle/Apps/vue-js-playground/udemy-lessons/vue-js-and-firebase/routing/src/components/Navbar.vue
@@ -4,14 +4,28 @@
       <li> <router-link :to="{name: 'Home'}"> Home </router-link> </li>
       <li> <router-link :to="{name: 'About'}"> About </router-link> </li>
     </ul>
+    <h2>Users</h2>
+    <ul>
+      <li v-for="(id, index) in userIds" :key="index">
+        <router-link :to="{name: 'ViewProfile', params: {user_id: id}}">
+          <span>
+            Profile {{ id }}             
+          </span>
+        </router-link>
+
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+
 export default {
   name: 'Navbar',
   data () {
     return {
+      userIds: ['1', '2', '3', '4', '5'],
+
     }
   }
 }
