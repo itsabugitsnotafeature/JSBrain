95686eba85a3ff716b47a9dd6adb6f1ec647be81
itsabugitsnotafeature <itsabugitsnotafeature@gmail.com>
2019-10-07 21:07:15 -0700

    Creating chat component

--- udizzle/Apps/vue-js-playground/udemy-lessons/vue-js-and-firebase/ninja-chat/src/components/Chat.vue
+++ udizzle/Apps/vue-js-playground/udemy-lessons/vue-js-and-firebase/ninja-chat/src/components/Chat.vue
@@ -1,6 +1,22 @@
 <template>
     <div class="chat">
-        Hello Chat => {{ this.name }}
+        <h2 class="center teal-text"> Ninja Chat </h2>
+        <div class="card">
+            <div class="card-content">
+                <ul class="messages">
+                    <li class=" ">
+                        <span class="teal-text">Name</span>
+                        <span class="grey-text text-darken-3">message</span>
+                        <span class="grey-text time">time</span>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="card-action">
+                <input type="text">
+            </div>
+        </div>
+
     </div>
 </template>
 
@@ -18,5 +34,17 @@ export default {
 
 
 <style scoped>
+.chat h2{
+    font-size: 2.6em;
+    margin-bottom: 40px;
+}
 
+.chat span {
+    font-size: 1.4em;
+}
+
+.chat .time {
+    display: block;
+    font-size: 1.2 em;
+}
 </style>
