56abfe41ea2305f4a3019213a598a41299375f30
itsabugitsnotafeature <itsabugitsnotafeature@gmail.com>
2019-10-07 22:15:03 -0700

    Implemented auto scroll

--- udizzle/Apps/vue-js-playground/udemy-lessons/vue-js-and-firebase/ninja-chat/src/components/Chat.vue
+++ udizzle/Apps/vue-js-playground/udemy-lessons/vue-js-and-firebase/ninja-chat/src/components/Chat.vue
@@ -3,7 +3,7 @@
         <h2 class="center teal-text">Ninja Chat</h2>
         <div class="card">
             <div class="card-content">
-                <ul class="messages">
+                <ul class="messages" v-chat-scroll>
                     <li v-for="message in messages" :key="message.id">
                         <span class="teal-text">{{message.name}}</span>
                         <span class="grey-text text-darken-3">{{message.content}}</span>
@@ -65,11 +65,17 @@ export default {
 }
 
 .chat span {
-    font-size: 0.8em;
+    font-size: 1.4em;
 }
 
 .chat .time {
     display: block;
     font-size: 0.8 em;
+}
+
+.messages {
+    max-height: 300px;
+    overflow: auto;
+
 }
 </style>
