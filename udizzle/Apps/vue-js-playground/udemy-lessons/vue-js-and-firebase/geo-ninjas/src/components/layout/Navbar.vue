92db917cbf93bc89e4a457e76a865021103c3cfa
itsabugitsnotafeature <itsabugitsnotafeature@gmail.com>
2019-11-05 22:33:19 -0800

    getting rid of small bugs

--- udizzle/Apps/vue-js-playground/udemy-lessons/vue-js-and-firebase/geo-ninjas/src/components/layout/Navbar.vue
+++ udizzle/Apps/vue-js-playground/udemy-lessons/vue-js-and-firebase/geo-ninjas/src/components/layout/Navbar.vue
@@ -1,25 +1,27 @@
 <template>
-    <div class="navbar">
-        <nav class="deep-purple darken-1">
-            <div class="container">
-                <a href="" class="brand-logo left">GeoNinjas!</a>
-                <ul class="right">
-                    <li>
-                        <a href="">Sign Up</a>
-                        <a href="">Login</a>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-    </div>    
+  <div class="navbar">
+    <nav class="deep-purple darken-1">
+      <div class="container">
+        <a href="" class="brand-logo left">GeoNinjas!</a>
+        <ul class="right">
+          <li>
+            <a href="">Sign Up</a>
+          </li>
+          <li>
+            <a href="">Login</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </div>    
 </template>
 
 <script>
 export default {
-    name: 'Navbar',
-    data () {
-        return {}
-    }
+  name: 'Navbar',
+  data () {
+    return {}
+  }
 }
 </script>
 
