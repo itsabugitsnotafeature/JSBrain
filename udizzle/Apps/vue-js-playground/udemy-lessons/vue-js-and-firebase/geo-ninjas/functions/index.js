667e51a39117aaadbeb71406a1f11716d6274451
itsabugitsnotafeature <itsabugitsnotafeature@gmail.com>
2019-11-09 10:59:49 -0800

    Created first cloud function to check if alias is unique

--- udizzle/Apps/vue-js-playground/udemy-lessons/vue-js-and-firebase/geo-ninjas/functions/index.js
+++ udizzle/Apps/vue-js-playground/udemy-lessons/vue-js-and-firebase/geo-ninjas/functions/index.js
@@ -1,4 +1,19 @@
+/* eslint-disable */
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+exports.checkAlias = functions.https.onCall((data, context) => {
+  const ref = admin.firestore().collection('users').doc(data.slug)
+  return ref.get().then(doc => {
+    return {
+      unique: !doc.exists
+    }
+  }).catch(err => {
+    throw new functions.https.HttpsError('failed to connect :-(')
+  })
+});
+
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
